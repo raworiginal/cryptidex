4501a927ea7c1bd894a08ff223b049d8564a1bea
raworiginal <randall.a.wolfe@gmail.com>
2025-07-29 08:25:47 -0700

    completed monster and attack schemas in models/monster.js

---
+++ models/monster.js
@@ -0,0 +1,33 @@
+/* ===================== Dependencies ===================== */
+const mongoose = require("mongoose");
+
+/* ===================== Models ===================== */
+
+const attackSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  harm: { type: Number, min: 1, required: true },
+  description: { type: String },
+});
+
+const monsterSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  description: { type: String },
+  image: { type: String },
+  armor: { type: Number, min: 0 },
+  attacks: [attackSchema],
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  favoritedByUsers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Monster = mongoose.model("Monster", monsterSchema);
+/* ===================== Exports ===================== */
+module.exports = Monster;

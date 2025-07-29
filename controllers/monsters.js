c5a188eda92d7d879812d2d07b53258b0b4cebee
raworiginal <randall.a.wolfe@gmail.com>
2025-07-29 09:10:37 -0700

    created monster/index.ejs and GET route for monsters/ in monsters controller

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -4,7 +4,20 @@ const router = express.Router();
 
 const Monster = require("../models/monster.js");
 
-/* ==================  ================== */
+/* ===================== CREATE ROUTES ===================== */
+/* ===================== READ ROUTES ===================== */
+router.get("/", async (req, res) => {
+  try {
+    const populatedMonsters = await Monster.find({}).populate("creator");
+    console.log(populatedMonsters);
+    res.render("monsters/index.ejs");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+/* ===================== UPDATE ===================== */
+/* ===================== DELETE ===================== */
 
 /* ================== Exports ================== */
 module.exports = router;

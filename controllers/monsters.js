dc2252fc46fabf6f45a1d80f2d3bcc4769e3a920
raworiginal <randall.a.wolfe@gmail.com>
2025-07-29 08:48:02 -0700

    created monster controller

---
+++ controllers/monsters.js
@@ -0,0 +1,12 @@
+/* ================== Dependencies ================== */
+const express = require("express");
+const router = express.Router();
+
+const Monster = require("../models/monster.js");
+
+/* ==================  ================== */
+
+/* ================== Exports ================== */
+module.exports = router;
+
+/* ==================  ================== */

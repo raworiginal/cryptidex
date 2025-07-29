6469721015b74623c7167bd56e8b3f35cd34ec16
raworiginal <randall.a.wolfe@gmail.com>
2025-07-29 13:02:38 -0700

    finished new monster POSTfunctionality

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -5,6 +5,25 @@ const router = express.Router();
 const Monster = require("../models/monster.js");
 
 /* ===================== CREATE ROUTES ===================== */
+router.get("/new", (req, res) => {
+  try {
+    res.render("monsters/new.ejs");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    req.body.creator = req.session.user._id;
+    await Monster.create(req.body);
+    console.log(req.body);
+    res.redirect("/monsters");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
 /* ===================== READ ROUTES ===================== */
 router.get("/", async (req, res) => {
   try {
